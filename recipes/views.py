a8dc1fdeed32c6f23ab72b6d5353e4f51ac6b36c
Владимир Буоц <vbuoc@yandex.ru>
2021-05-16 18:21:47 +0400

    tag filters (get)

--- recipes/views.py
+++ recipes/views.py
@@ -1,27 +1,32 @@
-from django.shortcuts import render
+from typing import List
+
 from django.views.generic import ListView
 
 from recipes.models import Recipe, Tag
 
 
 class RecipeBaseView(ListView):
-    # page_title = None
     paginate_by = 6
-    queryset = Recipe.objects.all()
+
+    def get_queryset(self):
+        tags = self.request.GET.getlist(
+            'tag',
+            ['breakfast', 'lunch', 'dinner']
+        )
+
+        return Recipe.objects.filter(
+            tags__title__in=tags
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_tags'] = Tag.objects.all()
+        return context
 
 
 class IndexView(RecipeBaseView):
     template_name = 'recipes/index.html'
     extra_context = {'title': 'Рецепты'}
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tags'] = Tag.objects.all()
-        return context
-
-    def get_queryset(self):
-        tags = self.request.GET.getlist('tag')
-        return Recipe.objects.filter(tags__title__in=tags)
 
 
 class RecipeViewSlug(ListView):
