882a65df35d6974131807d616f6dbf7fb574055b
Владимир Буоц <vbuoc@yandex.ru>
2021-05-19 00:30:35 +0400

    api (api ingredients) | есть проблема, не работает стандартный фильтр. Сделал через queryset

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-
 from api.views import IngredientViewSet
 
-router = DefaultRouter()
-router.register(r'ingredients', IngredientViewSet, basename='ingredients')
+router_v1 = DefaultRouter()
+router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,21 @@
 from rest_framework.mixins import ListModelMixin
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import GenericViewSet
-from rest_framework import filters
 
 from api.serializers import IngredientSerializer
+
 from recipes.models import Ingredient
 
 
 class IngredientViewSet(ListModelMixin, GenericViewSet):
-    queryset = Ingredient.objects.all()
+    # queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    filter_backends = (filters.SearchFilter, )
-    search_fields = ('^title',)
+    filter_backends = [SearchFilter]
+    search_fields = ['^title', ]
+
+    def get_queryset(self):
+        data = self.request.GET['query']
+        if data is not None:
+            queryset = Ingredient.objects.filter(title__istartswith=data)
+            return queryset
+        return Ingredient.objects.none()
